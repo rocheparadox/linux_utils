1f85f4b3e34bf93ea213a3826670785b5169bf31
Roche Periyanayagam <rocheinside@gmail.com>
2019-03-13 16:15:19 +0530

    bug fix in os identifier

--- identifier_os.sh
+++ identifier_os.sh
@@ -1,18 +1,16 @@
 #This script has to be run as /bin/bash not /bin/dash
 #This script is used to determine if a system has ubuntu or mac os
 
-{
 if [ -e /etc/os-release  ]; then 
 	determiner=$(cat /etc/os-release)
+else
+	determiner=$(sw_vers)
 fi
-} ||
-{
-determiner=$(sw_vers)
-}
+
 
 if [[ $determiner == *Ubuntu*  ]]; then
 	echo "It is Ubuntu" 
 
-else [[ $determiner == *MAC*  ]]; 
+elif [[ $determiner == *Mac*  ]]; then 
 	echo "It is MAC OS X"
 fi
