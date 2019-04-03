4fb1f23135a4c8e5a4758f68d3c98cf520f84055
Roche Periyanayagam <rocheinside@gmail.com>
2019-04-03 19:49:39 +0530

    Grep the touchpad id from xinput instead of user providing it

--- mousepadOff.sh
+++ mousepadOff.sh
@@ -1,8 +1,3 @@
-
-if [ -z $1 ]; then
-    echo "Please enter the device id as first argument after running xinput command"
-    exit
-fi
-
-deviceId=$1
+#!/bin/bash
+deviceId=$(echo $(xinput) | grep -P -o "(?<=TouchPad.id=)([0-9]*)")
 xinput set-prop $deviceId "Device Enabled" 0
