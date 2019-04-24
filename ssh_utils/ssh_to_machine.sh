f0a7f246d3371ce0bdbcad1e5bae9b327dcff54e
Roche Periyanayagam <rocheinside@gmail.com>
2019-04-24 15:05:04 +0530

    abs path in ssh_to_machine.sh

--- ssh_utils/ssh_to_machine.sh
+++ ssh_utils/ssh_to_machine.sh
@@ -2,12 +2,13 @@ machine_name=$1
 username=$2
 password=$3
 
+file_path=$( cd $( dirname $0 ); pwd )
 if [ "$1" = "--help" ] || [ -z "$1"  ] || [ -z "$2"  ] || [ -z "$3" ] ; then 
 
 echo "Requires 3 arguments \nssh_to_machine [machine_name] [username] [password]  "
 
 else
 
-expect ssh_to_machine.expect $username $machine_name $password
+expect $file_path/ssh_to_machine.expect $username $machine_name $password
 
 fi
